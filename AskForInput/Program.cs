b25d7b832113600ec657336958ec9426e0939da8
Daniel Silva <danielhull.silva1@gmail.com>
2024-02-23 23:16:13 +0000

    Agora pediu-se ao utilizador que este coloque um número real ao colocar os dois números aparece a soma

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,14 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insira um numero inteiro");
+            string str = Console.ReadLine();
+            int i = int.Parse(str);
+            Console.WriteLine("Insira um numero real");
+            str = Console.ReadLine();
+            float f = float.Parse(str);
+            Console.WriteLine(f+i);
+
         }
     }
 }
