5d37c6cdb0a02fd10e890822a06d139bc5ae9aa6
Daniel Silva <danielhull.silva1@gmail.com>
2024-02-23 22:33:01 +0000

    Adicionou-se mais stringes como pedido no exercício 5

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,6 +12,17 @@ namespace ManyStrings
             string C = "Good Morning!";
             string D = "simbolos \u2767 \u1F41 \u1F90 ";
             Console.WriteLine(A + " " + B + " " + C + " " + " "+ D);
+
+            //Ex5
+            string E = "Cão " + 3 + "Gato";
+            string F = "Ouvi dizer" + C;
+            string G = "Levanta-te " + "e " + "Ri";
+            string H = "um simbolo " + A + " mais " + D;
+            E += " x vezes ";
+            string I = $"{A} mais {C} é igual a {A+C}";
+            string J = $"pasta {B}";
+            Console.WriteLine(E + "  " + F + "  " + G + "  " + H +
+             "  " + I + "  " + J);
         }
     }
 }
