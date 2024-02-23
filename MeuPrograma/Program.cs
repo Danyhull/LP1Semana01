f6f0b2ad9662aff22e7c0ad951766a4339c9d8e4
Daniel Silva <danielhull.silva1@gmail.com>
2024-02-23 10:31:12 +0000

    Add de as tags /// para adicionar informação no Main e na class programa

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// o programa executa a função do hello world e bye world
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Esta função diz Hello world e Bye,World
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
