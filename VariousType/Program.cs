41f2be66800c0da6fefa4bdd5face0a873612aa4
Daniel Silva <danielhull.silva1@gmail.com>
2024-02-23 10:59:14 +0000

    Adicionou-se 5 variáveis

---
+++ VariousType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            sbyte var1 = -128;
+            byte var2 = 255;
+            short var3 = -32768;
+            ushort var4 = 65535;
+            int var5 = 2
+        }
+    }
+}
