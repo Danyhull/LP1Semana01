6a35d31b681a881b6713c0a511c510ceb44c53fc
Daniel Silva <danielhull.silva1@gmail.com>
2024-02-23 14:03:20 +0000

    Adicionou-se várias strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex4
+            string A = "\u261A";
+            string B = "Good\\Night \u263E";
+            string C = "Good Morning!";
+            string D = "simbolos \u2767 \u1F41 \u1F90 ";
+            Console.WriteLine(A + " " + B + " " + C + " " + " "+ D);
+        }
+    }
+}
